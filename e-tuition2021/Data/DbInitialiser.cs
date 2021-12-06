f4fa134b225b3e12868f281e03bac60467f17b8c
Mohammed Ahmed <reazssj4@gmail.com>
2021-12-06 07:45:22 +0000

    added more classes to DbInitialiser

--- e-tuition2021/Data/DbInitialiser.cs
+++ e-tuition2021/Data/DbInitialiser.cs
@@ -48,7 +48,7 @@ namespace e_tuition2021.Data
             };
 
             context.Addresses.AddRange(addresses);
-            context.SaveChanges();
+            //context.SaveChanges();
         }
 
         private static void AddPaymentCards(ApplicationDbContext context)
@@ -73,7 +73,7 @@ namespace e_tuition2021.Data
             };
 
             context.PaymentCards.AddRange(paymentcards);
-            context.SaveChanges();
+            //context.SaveChanges();
         }
 
         private static void AddPeople(ApplicationDbContext context)
@@ -110,8 +110,17 @@ namespace e_tuition2021.Data
 
             var tutor = new Tutor[]
             {
-                new Tutor {}
+                new Tutor {Degree="Maths", MobileNumber="009746789345673", Bio=" ", Cost=17, PGCE=true},
+                new Tutor {Degree="English", MobileNumber="056352673898843", Bio=" ", Cost=15, PGCE=true},
+                new Tutor {Degree="Biology", MobileNumber="023456278645638", Bio=" ", Cost=15, PGCE=true},
+                new Tutor {Degree="Chemistry", MobileNumber="017845537284563", Bio=" ", Cost=15, PGCE=true},
+                new Tutor {Degree="Physics", MobileNumber="045647783954623", Bio=" ", Cost=19, PGCE=true},
+                new Tutor {Degree="Maths", MobileNumber="089465367992345", Bio=" ", Cost=20, PGCE=true},
+                new Tutor {Degree="English", MobileNumber="034566273564825", Bio=" ", Cost=15, PGCE=true}
             };
+
+            context.Tutors.AddRange(tutor);
+            //context.SaveChanges();
         }
 
         private static void AddStaff(ApplicationDbContext context)
@@ -123,8 +132,14 @@ namespace e_tuition2021.Data
 
             var staff = new Staff[]
             {
-                new Staff {JobTitle=""}
+                new Staff {JobTitle="Lead Programmer", Salary=50000},
+                new Staff {JobTitle="Designer", Salary=20000},
+                new Staff {JobTitle="Project Manager", Salary=15000},
+                new Staff {JobTitle="Technician", Salary=18000}
             };
+
+            context.Staff.AddRange(staff);
+            //context.SaveChanges();
         }
 
         private static void AddLessons(ApplicationDbContext context)
@@ -136,8 +151,21 @@ namespace e_tuition2021.Data
 
             var lessons = new Lesson[]
             {
+                new Lesson {LessonId=1, TimeSlotId=1, PersonId=1, StudentId=1, KeyStage=4, FaceToFace=true},
+                new Lesson {LessonId=2, TimeSlotId=2, PersonId=2, StudentId=2, KeyStage=3, Online=true},
+                new Lesson {LessonId=3, TimeSlotId=3, PersonId=3, StudentId=3, KeyStage=2, FaceToFace=true},
+                new Lesson {LessonId=4, TimeSlotId=4, PersonId=4, StudentId=4, KeyStage=1, Online=true},
+                new Lesson {LessonId=5, TimeSlotId=5, PersonId=5, StudentId=5, KeyStage=1, FaceToFace=true},
+                new Lesson {LessonId=6, TimeSlotId=6, PersonId=6, StudentId=6, KeyStage=2, Online=true},
+                new Lesson {LessonId=7, TimeSlotId=7, PersonId=7, StudentId=7, KeyStage=3, FaceToFace=true},
+                new Lesson {LessonId=8, TimeSlotId=8, PersonId=8, StudentId=8, KeyStage=4, Online=true},
+                new Lesson {LessonId=9, TimeSlotId=9, PersonId=9, StudentId=9, KeyStage=4, FaceToFace=true},
+                new Lesson {LessonId=10, TimeSlotId=10, PersonId=10, StudentId=10, KeyStage=3, Online=true}
 
             };
+
+            context.Lessons.AddRange(lessons);
+            //context.SaveChanges();
         }
 
         private static void AddStudents(ApplicationDbContext context)
@@ -149,8 +177,20 @@ namespace e_tuition2021.Data
 
             var students = new Student[]
             {
-
+                new Student {Id=1, KeyStage=3, ParentId=1, TutorId=1},
+                new Student {Id=2, KeyStage=4, ParentId=2, TutorId=2},
+                new Student {Id=3, KeyStage=2, ParentId=3, TutorId=3},
+                new Student {Id=4, KeyStage=1, ParentId=4, TutorId=4},
+                new Student {Id=5, KeyStage=1, ParentId=5, TutorId=5},
+                new Student {Id=6, KeyStage=2, ParentId=6, TutorId=6},
+                new Student {Id=7, KeyStage=3, ParentId=7, TutorId=7},
+                new Student {Id=8, KeyStage=4, ParentId=8, TutorId=8},
+                new Student {Id=9, KeyStage=4, ParentId=9, TutorId=9},
+                new Student {Id=10, KeyStage=3, ParentId=10, TutorId=10}
             };
+
+            context.Students.AddRange(students);
+            //context.SaveChanges();
         }
 
         private static void AddTimeSlots (ApplicationDbContext context)
@@ -160,13 +200,25 @@ namespace e_tuition2021.Data
                 return;
             }
 
-            var timeSlots = new TimeSlot[]
+            var timeslots = new TimeSlot[]
             {
-
+                new TimeSlot{TutorId=1, Booked=true, DayOfTheWeek=12, StartHour=0900, EndHour=1000},
+                new TimeSlot{TutorId=2, Booked=true, DayOfTheWeek=15, StartHour=1200, EndHour=1300},
+                new TimeSlot{TutorId=3, Booked=true, DayOfTheWeek=18, StartHour=1000, EndHour=1100},
+                new TimeSlot{TutorId=4, Booked=true, DayOfTheWeek=21, StartHour=1400,EndHour=1500},
+                new TimeSlot{TutorId=5, Booked=true, DayOfTheWeek=24, StartHour=0930, EndHour=1030},
+                new TimeSlot{TutorId=6, Booked=true, DayOfTheWeek=27, StartHour=1030, EndHour=1130},
+                new TimeSlot{TutorId=7, Booked=true, DayOfTheWeek=30, StartHour=1230, EndHour=1300},
+                new TimeSlot{TutorId=8, Booked=true, DayOfTheWeek=14, StartHour=1410, EndHour=1510},
+                new TimeSlot{TutorId=9, Booked=true, DayOfTheWeek=22, StartHour=0900, EndHour=1030},
+                new TimeSlot {TutorId=10, Booked=true, DayOfTheWeek=29, StartHour=1330, EndHour=1430}
             };
+
+            context.TimeSlot.AddRange(timeslots);
+            //context.SaveChanges();
         }
 
-        
+
 
 
     }
