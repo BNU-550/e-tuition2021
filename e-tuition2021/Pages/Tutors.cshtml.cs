d9fcfb64c5d326048871a72bd9848c26855ea4d2
hakee <hakee@LAPTOP-84U9VAUD>
2021-12-29 16:59:33 +0000

    design

---
+++ e-tuition2021/Pages/Tutors.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace e_tuition2021.Pages
+{
+    public class TutorsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
