3e1e528daccbd808713fc333bd81392eaf0edf65
Mohammed Ahmed <reazssj4@gmail.com>
2022-01-10 06:51:42 +0000

    added payment card crud page

---
+++ e-tuition2021/Pages/PaymentCards/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using e_tuition2021.Data;
+using e_tuition2021.Models;
+
+namespace e_tuition2021.Pages.PaymentCards
+{
+    public class IndexModel : PageModel
+    {
+        private readonly e_tuition2021.Data.ApplicationDbContext _context;
+
+        public IndexModel(e_tuition2021.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<PaymentCard> PaymentCard { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            PaymentCard = await _context.PaymentCards.ToListAsync();
+        }
+    }
+}
